e8be5ccf58d18dac26c23953aec38a84cf1a3fed
Mikolaj Grobelny <miki4920@gmail.com>
2020-06-28 17:21:18 +0100

    Created a basic loop, program starts and finishes normally TODO: Figure out actual fishing detection, separate key handling into separate file

--- TerrariaFishingBot.py
+++ TerrariaFishingBot.py
@@ -1,8 +1,40 @@
-from pynput.mouse import Button, Controller
+from pynput import keyboard
+from pynput.mouse import Controller, Button
 import time
-mouse = Controller()
 
-while True:
-    mouse.press(Button.left)
-    time.sleep(0.15)
-    mouse.release(Button.left)
+
+class TerraBot(object):
+    def __init__(self):
+        self.on = False
+        self.mouse = Controller()
+        listener = keyboard.Listener(
+            on_release=self.on_release)
+        listener.start()
+        self.start_program()
+
+    def on_release(self, key):
+        try:
+            if key.char == "f":
+                self.on = not self.on
+        except AttributeError:
+            pass
+
+    def start_program(self):
+        while True:
+            while self.on:
+                self.fishing_loop()
+                break
+
+    def fishing_loop(self):
+        self.mouse.press(Button.left)
+        time.sleep(0.01)
+        self.mouse.release(Button.left)
+        time.sleep(2)
+
+
+
+
+
+
+
+terraBot = TerraBot()
