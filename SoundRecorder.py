4e1f2f2a030e86f76666268297c76af4de89ff4d
Mikolaj Grobelny <miki4920@gmail.com>
2020-07-09 14:46:30 +0100

    Added comments to two other programs. Finished Program.

--- SoundRecorder.py
+++ SoundRecorder.py
@@ -10,25 +10,31 @@ RECORD_SECONDS = 10
 
 class SoundRecorder(object):
     def __init__(self, volume=18500):
+        # Initialises recorder, gets an index of Stereo Mix Device
         self.p = pyaudio.PyAudio()
         self.dev_index = self.get_dev_index()
         self.volume = volume
 
     def get_dev_index(self):
+        # Looks through all devices and searches for Stereo Mix
         for i in range(self.p.get_device_count()):
             dev = self.p.get_device_info_by_index(i)
             if dev['name'] == 'Stereo Mix (Realtek(R) Audio)' and dev['hostApi'] == 0:
                 return dev['index']
 
     def check_volume(self):
+        # Open stream
         stream = self.p.open(format=FORMAT,
                              channels=CHANNELS,
                              rate=RATE,
                              input=True,
                              input_device_index=self.dev_index,
                              frames_per_buffer=CHUNK)
+        # Second sound variable required to ignore fishing rod sounds
         second_sound = False
         for i in range(0, int(RATE/CHUNK*RECORD_SECONDS)):
+            # Iterates through incoming sound, converts it to a linear scale and checks if the sound is louder than
+            # current volume
             data = stream.read(CHUNK)
             rms = audioop.rms(data, 2)
             if rms > self.volume:
--- TerrariaFishingBot.py
+++ TerrariaFishingBot.py
@@ -10,16 +10,20 @@ class TerraBot(object):
         self.mouse = Controller()
 
     def click(self, button):
+        # A function responsible for clicking, 0.05 delay so the game can detect click
         self.mouse.press(button)
         time.sleep(0.05)
         self.mouse.release(button)
 
     def start_program(self):
+        # Continuous loop which only executes fishing part when program is on
         while True:
             while self.on:
                 self.fishing_loop()
 
     def fishing_loop(self):
+        # Throws the float, listens for volume, catches fish and then waits two seconds to repeat again (Reeling
+        # takes approximately 2 seconds)
         self.click(Button.left)
         if self.sound_recorder.check_volume():
             self.click(Button.left)
